27b5fa8cc46e08d113f68680ea4c26fc97d110ba
Joe Eagar <joeedh@gmail.com>
2014-04-22 02:53:09 -0700

    updated git_pull again

--- git_pull.sh
+++ git_pull.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 git pull
-echo "Making sure parse tables are up to date..."
+echo "---Making sure parse tables are up to date..."
 cd tools/extjs_cc
 python3.3 js_cc.py 2> /dev/null
 cd ../../
 
-echo "Executing build. . ."
+echo "---Executing build. . ."
 python3.3 js_build.py
 
-echo "Restarting httpd. . ."
+echo "---Restarting httpd. . ."
 sudo /etc/init.d/httpd restart
 
 
